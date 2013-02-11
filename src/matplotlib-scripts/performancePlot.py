7a7c7061db4f9f209717aa6afb7352e3bf8ef442
Victoria Caparros <caparrov@inf.ethz.ch>
2013-02-11 12:02:48 +0100

    Updated performancePlot.py

--- src/matplotlib-scripts/performancePlot.py
+++ src/matplotlib-scripts/performancePlot.py
@@ -4,7 +4,6 @@ import sys
 import os
 import math
 from matplotlib import rc
-rc('text', usetex=True) # this is if you want to use latex to print text. If you do you can create strings that go on labels or titles like this for example (with an r in front): r"$n=$ " + str(int(n))
 from numpy import *
 from pylab import *
 import random
@@ -28,25 +27,36 @@ matplotlib.rc('xtick.major',size =0 )
 matplotlib.rc('xtick.minor',size =0 )
 matplotlib.rc('ytick.major',size =0 )
 matplotlib.rc('ytick.minor',size =0 )
+#matplotlib.rc('font', family='sans-serif')
+matplotlib.rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+## for Palatino and other serif fonts use:
+#rc('font',**{'family':'serif','serif':['Palatino']})
+matplotlib.rc('text', usetex=True)
 
 
-X_MIN=10
-X_MAX=1000000
+X_MIN=0.1
+X_MAX=1024
 Y_MIN=0
-Y_MAX=1
+Y_MAX=2
 LOG_X=1
 LOG_Y=0
 TITLE="TITLE"
 X_LABEL="Problem Size [Complex Doubles]"
 Y_LABEL="Performance [Flop/Cycle]"
-OUTPUT_FILE="errorPlot.pdf"
+OUTPUT_FILE="performancePlot.pdf"
+PLOT_STATS=1
+colors=['black', '#000066','#336600','#CC0033' ,'#FFFF00' ]
+series = ['MKL', 'MMM']
+
 
 fig = plt.figure()
-# Returns the Axes instance
 ax = fig.add_subplot(111)
-ffont = {'family':'sans-serif','fontsize':10,'weight':'bold'}
-ax.set_xticklabels(ax.get_xticks(),ffont)
-ax.set_yticklabels(ax.get_yticks(),ffont)
+
+#TODO: fix the font familiy of the tick labels. Everythong I have tried so far does not work...
+#ffont = {'family':'sans-serif','fontsize':10,'weight':'bold'}
+#ax.set_xticklabels(ax.get_xticks(),ffont)
+#ax.set_yticklabels(ax.get_yticks(),ffont)
+
 
 #Log scale
 if LOG_Y:  ax.set_yscale('log')
@@ -62,43 +72,109 @@ ax.set_ylabel(Y_LABEL, fontsize=12)
 ax.axis([X_MIN,X_MAX,Y_MIN,Y_MAX])
 
 #TODO: When log scale, axis labels have a different font...
-ticks_font = matplotlib.font_manager.FontProperties(family='Helvetica', style='normal', size=12, weight='normal', stretch='normal')
 if LOG_X:
 	ax.xaxis.set_major_formatter(matplotlib.ticker.ScalarFormatter())
 	#ax.xaxis.major.formatter.set_powerlimits((-3, 6)) 
-	for label in ax.get_xticklabels() :
-		label.set_fontproperties(ticks_font)
+#	for label in ax.get_xticklabels() :
+#		label.set_fontproperties(ticks_font)
 
-if LOG_X:
+if LOG_Y:
         ax.yaxis.set_major_formatter(matplotlib.ticker.ScalarFormatter())
         #ax.yaxis.major.formatter.set_powerlimits((-3, 6)) 
-        for label in ax.get_yticklabels() :
-                label.set_fontproperties(ticks_font)
-
-# Load the data --- At the moment, false data, need to figure our 
-# how to read the data depending on the format
-x1=[0.5,0.6,0.7,0.8,0.5,0.7]
-x2=[0.5,0.6,0.7,0.8,0.5,0.7]
-x3=[0.5,0.5,0.5,0.5,0.5,0.5]
-mediansX = [100.0,1000.0,10000.0]
-mediansY = [0.65,0.65,0.5]
-
-# Plot a line between the medians of each dataset
-ax.plot(mediansX, mediansY, '-', lw=1,color='black', label="serie 1" )
+#        for label in ax.get_yticklabels() :
+#                label.set_fontproperties(ticks_font)
 
 
-#Percentile boxes
-locs, labels = xticks() 
-rectanglesWidths = [x/3 for x in mediansX]
-bp = ax.boxplot([x1,x2,x3], positions=mediansX, widths = rectanglesWidths)
-setp(bp['medians'], color='black')
-setp(bp['fliers'], color='black')
-setp(bp['whiskers'], color='black', linestyle= '-')
-setp(bp['boxes'], color='black')
-setp(bp['caps'], color='black')
 
-#Restore xticks location --> Needed to locate boxes in arbitrary positions
-xticks(locs)
+
+
+
+# Load the data 
+
+for serie,i in zip(series,range(len(series))):
+	
+	nCycles = []
+	file_in = open('tsc_'+serie+'.txt','r')
+	lines = file_in.readlines()
+	for line in lines:
+		split_line = line.rstrip('\n').split(' ')
+		nCycles.append(split_line)
+
+	file_in.close()
+
+
+
+	nFLOPS = []
+	file_in = open('flop_'+serie+'.txt','r')
+	lines = file_in.readlines()
+	for line in lines:
+        	split_line = line.rstrip('\n').split(' ')
+        	nFLOPS.append(split_line)
+
+	file_in.close()
+
+
+
+	yData =[]
+	for f,c in zip(nFLOPS,nCycles):
+		yData.append([float(vf)/float(vc) for vf, vc in zip(f,c) if vf != '' and vc != ''])
+
+
+
+	xData = []
+	file_in = open('size_'+serie+'.txt','r')
+	lines = file_in.readlines()
+	for line in lines:
+        	split_line = line.rstrip('\n').split(' ')
+        	xData.append(split_line)
+
+	file_in.close()
+
+	xData = [float(x) for x in xData[0]]
+
+	if PLOT_STATS:	
+		"""
+		Percentile boxes
+		On each box, the central mark is the median, the edges of the box are the lower hinge (defined as the 25th percentile
+		) and the upper hinge (the 75th percentile), the whiskers extend to the most extreme data points not considered outliers,
+	 	this ones are plotted individually.	
+		"""
+		locs, labels = xticks() 
+		if LOG_X:
+			rectanglesWidths = [float(x)/3 for x in xData]
+			bp = ax.boxplot(yData, positions=xData, widths = rectanglesWidths)
+		else:
+			bp = ax.boxplot(yData, positions=xData)
+		setp(bp['medians'], color='none')
+		setp(bp['fliers'], color=colors[i],marker='None')
+		setp(bp['whiskers'], color=colors[i], linestyle= '-')
+		setp(bp['boxes'], color=colors[i])
+		setp(bp['caps'], color=colors[i])
+
+		#Restore xticks location --> Needed to locate boxes in arbitrary positions
+		xticks(locs)
+
+
+		# Plot a line between the medians of each dataset => need to get medians calculated by boxplot
+		medians = range(len(xData))
+		for j in range(len(xData)):
+			med = bp['medians'][j]
+			medianX = []
+  			medianY = []
+  			for k in range(2):
+    	  			medianX.append(med.get_xdata()[k])
+      				medianY.append(med.get_ydata()[k])
+      				medians[j] = medianY[0]	
+
+
+		ax.plot(xData, medians, '-', lw=1,color=colors[i], marker='o', markeredgecolor=colors[i], markersize=4, label=serie)
+
+	else:
+		ax.plot(xData, yData, '-', lw=1,color=colors[i], marker='o', markeredgecolor=colors[i], markersize=4, label=serie)	
+	
+	# end of if-else
+
+#end for loop
 
 
 ax.legend(numpoints=1, loc='best',fontsize =6,frameon = False )
