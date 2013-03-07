73b61e0829c7abc1a788460909556b8563749c71
ofgeorg <ofgeorg@lemming.(none)>
2013-03-07 14:22:49 +0100

    spec bench

--- pcm/measuring_core.h
+++ pcm/measuring_core.h
@@ -3,19 +3,21 @@
 
 
 
-
+#ifdef __cplusplus
+extern "C" {
+#endif
 //int measurement_init(int type, bool flushData , bool flushICache , bool flushTLB );
-int measurement_init(long * custom_counters = NULL, unsigned long offcore_response0 = 0, unsigned long offcore_response1 = 0);
+int measurement_init(long * custom_counters , unsigned long offcore_response0 , unsigned long offcore_response1 );
 void measurement_start();
-void measurement_stop(unsigned long runs=1);
+void measurement_stop(unsigned long runs);
 void measurement_end();
 // Start Dani
 //bool measurement_customTest(size_t runs, size_t vlen);
 unsigned long measurement_run_multiplier(unsigned long threshold);
-bool measurement_testDerivative(size_t runs, double alpha_threshold, double avg_threshold, double time_threshold, double *d, size_t points=1);
+bool measurement_testDerivative(size_t runs, double alpha_threshold, double avg_threshold, double time_threshold, double *d, size_t points);
 //void measurement_meanSingleRun();
 //bool measurement_testSD(size_t runs);
-void measurement_emptyLists(bool clearRuns=true);
+void measurement_emptyLists(bool clearRuns);
 void dumpMeans();
 
 unsigned long measurement_getNumberOfShifts(unsigned long size, unsigned long initialGuess);
@@ -27,7 +29,9 @@ void flushICache();
 void flushDCache();
 
 
-
+#ifdef __cplusplus
+}
+#endif
 #endif
 
 
